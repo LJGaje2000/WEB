dc732cb6b41b3e4860be4e0e690f1dc9a5fc211e
LJGaje2000 <ljgaje.r6@gmail.com>
2023-06-24 23:25:29 +0530

    WEB DEVELOPMENT  IT-WEB-22-61-0014-G.M.L.J. Gajendrasingha
    WEB DEVELOPMENT  IT-WEB-22-61-0014-G.M.L.J. Gajendrasingha

---
+++ WEB DEVELOPMENT  IT-WEB-22-61-0014-G.M.L.J. Gajendrasingha/Q2/Q2 WebApplication/Pages/Add New Course/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Q2_WebApplication.Pages.Add_New_Course
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
